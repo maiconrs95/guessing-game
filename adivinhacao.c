ba4fac2a37a3edcefb4f4e7ad5ffacdd8e7c1b88
Maicon Silva <maiconrs95@gmail.com>
2021-09-05 01:29:10 -0300

    feat: game loops

--- adivinhacao.c
+++ adivinhacao.c
@@ -10,7 +10,30 @@ int main()
     int secret_number = 59;
     int chute;
 
-    printf("Qual é o seu chute? ");
-    scanf("%d", &chute);
-    printf("Seu chute foi %d ", chute);
+    for (int i = 1; i <= 3; i++)
+    {
+        printf("Tentativa %d de %d \n", i, 3);
+        printf("Qual é o seu chute? \n");
+        scanf("%d", &chute);
+
+        int acertou = (chute == secret_number);
+
+        if (acertou)
+        {
+            printf("Awe, Você acertou!!! O número secreto é: %d \n", chute);
+        }
+        else
+        {
+            int maior = chute > secret_number;
+
+            if (maior)
+            {
+                printf("Seu chute foi maior que o número secreto, tente novamente!\n");
+            }
+            else
+            {
+                printf("Seu chute foi menor que o número secreto, tente novamente!\n");
+            }
+        }
+    }
 }
